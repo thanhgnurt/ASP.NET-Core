47b53ec2d0305c6f01852517dbba6cfda910b536
Thanh Trung <thanhgnurt@gmail.com>
2021-01-05 22:03:00 +0700

    commit first

---
+++ ASP_Web_Core/ASP_Web_Core/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ASP_Web_Core
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //đăng ký các dịch vu (service)
+            //cung cấp chức năng cho ứng dụng
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //dùng đăng ký các middleware
+            //----------------------
+
+            //-cho phép sử dụng file tĩnh
+            //-chỉ định file được cung cấp mặc định khi url không rõ địa chỉ
+            var options = new DefaultFilesOptions();
+            options.DefaultFileNames.Add("home.html"); //tham số thêm file mặc định
+            options.DefaultFileNames.Add("home.htm");
+            app.UseDefaultFiles(options);
+            app.UseStaticFiles();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
---
+++ ASP_Web_Core/ASP_Web_Core/wwwroot/Script.js
@@ -0,0 +1,1 @@
+﻿alert("This is an alert from JavaScript");
